b5dbf208d0659418c275736ebab39f8612cffd5e
kuko <48900301+xukechun@users.noreply.github.com>
2019-10-19 17:29:32 +0800

    Update dmps.cpp

--- dmp_ws/src/dmp/nodes/dmps.cpp
+++ dmp_ws/src/dmp/nodes/dmps.cpp
@@ -3,10 +3,15 @@
 #include <visualization_msgs/Marker.h>
 
 #include <cmath>
+#include <fstream>
+#include <sstream>
+#include <string>
 
+using namespace std;
 using namespace dmp;
 
 std::vector<DMPData> active_dmp_list;
+DMPTraj raw_plan;
 
 //client object
 dmp::LearnDMPFromDemo mydemo;
@@ -16,50 +21,89 @@ dmp::SetActiveDMP myactive_dmp;
 void Set_mydemo()
 {
 	ROS_INFO("mydemo is begin.");
+
 	//Initialize all the points
-	double p1[3] = {0,0,0};
-	double p2[3] = {1,1,1};
-	double p3[3] = {2,3,4};
-	double v1[3] = {0,0,0};
-	double v2[3] = {0,0,0};
-	double v3[3] = {0,0,0};
-	double time[3] = {0,1,2};
+	std::vector<DMPPoint> pnts;
+	int max_index;
+	int index = 0;
+	string temp0,temp1;
 
-	DMPPoint pnt1;
-	pnt1.positions.insert(pnt1.positions.begin(),p1+1,p1+3);
-	pnt1.velocities.insert(pnt1.velocities.begin(),v1+1,v1+3);
-	ROS_INFO("pnt1 is ready.");	
+	//left_elbow
+	ifstream mydata1,mydata0;
+	mydata1.open("/home/xu/dmp_ws/src/dmp/data/data3/right_hand.txt");
+	//mydata0.open("/home/xu/dmp_ws/src/dmp/data1/right_shoulder.txt");
+	max_index = 171;
+	if(!mydata1.is_open())
+	{
+		ROS_INFO("fail to open the data!");
+	}
+	while(getline(mydata1,temp1))
+	{
+		if(index>=0 && index <max_index)
+		{
+			DMPPoint p;
+			double a[3] = {50,50,50};
+			istringstream iss0,iss1;
+			//iss0.str(temp0);
+			iss1.str(temp1);
+			string s0,s1;
+			int i = 0;
+			while(iss1 >> s1)
+			{
+				//double x0 = atof(s0.c_str());
+				double x1 = atof(s1.c_str());
+				a[i] = x1;
+				cout << x1 << endl;
+				i++;
+			}
+			p.positions.insert(p.positions.begin(),a,a+3);
+			if(index == 0)
+			{
+				double v[3] = {0};
+				p.velocities.insert(p.velocities.begin(),v,v+3);
+			}
+			else
+			{
+				double v[3] = {0};
+				p.velocities.insert(p.velocities.begin(),v,v+3);				
+			}
+			pnts.push_back(p);
+			//ROS_INFO("%d pnts over!",index + 1);
+		}
+		index++;
+	}
+	//ROS_INFO("load pnts over!");
+	for(int i=0; i<max_index; i++)
+	{
+		mydemo.request.demo.points.push_back(pnts[i]);
+	}
+	//ROS_INFO("load pnts over again!");
+	//total time is 10s
+	std::vector<double> time;
+	for(int i=0; i<max_index; i++)
+	{
+		time.push_back(i*171/(max_index-1));
+	}
+	
 
-	DMPPoint pnt2;
-	pnt2.positions.insert(pnt2.positions.begin(),p2+1,p2+3);
-	pnt2.velocities.insert(pnt2.velocities.begin(),v2+1,v2+3);
-	ROS_INFO("pnt2 is ready.");	
-
-	DMPPoint pnt3;
-	pnt3.positions.insert(pnt3.positions.begin(),p3+1,p3+3);
-	pnt3.velocities.insert(pnt3.velocities.begin(),v3+1,v3+3);
-	ROS_INFO("pnt3 is ready.");	
-
-	mydemo.request.demo.points.push_back(pnt1);
-	mydemo.request.demo.points.push_back(pnt2);
-	mydemo.request.demo.points.push_back(pnt3);	
-
-	mydemo.request.demo.times.insert(mydemo.request.demo.times.begin(),time+1, time+3);
+	mydemo.request.demo.times.insert(mydemo.request.demo.times.begin(),time.begin(),time.begin()+max_index);
+	//ROS_INFO("dims of my demo: %d",mydemo.request.demo.points[0].positions.size());
 	ROS_INFO("demo points : %d",mydemo.request.demo.points.size());
-	ROS_INFO("demo is ready.");	
+	//ROS_INFO("demo is ready.");	
 
 	//coefficient of elasticity and coefficient of resistance of each DOF
-	mydemo.request.k_gains.push_back(1);
-	mydemo.request.k_gains.push_back(1);
-	mydemo.request.k_gains.push_back(1);
-	mydemo.request.d_gains.push_back(1);
-	mydemo.request.d_gains.push_back(1);
-	mydemo.request.d_gains.push_back(1);
-	ROS_INFO("kd is ready.");	
+	//should be modified!!!!!
+	mydemo.request.k_gains.push_back(10);
+	mydemo.request.k_gains.push_back(10);
+	mydemo.request.k_gains.push_back(10);
+	mydemo.request.d_gains.push_back(10);
+	mydemo.request.d_gains.push_back(10);
+	mydemo.request.d_gains.push_back(10);
+	//ROS_INFO("kd is ready.");	
 
 	//this para is not used
 	mydemo.request.num_bases = 1;
-	ROS_INFO("all is ready.");
+	//ROS_INFO("all is ready.");
 
 	mydemo.response.dmp_list = active_dmp_list;
 
@@ -72,15 +116,16 @@ void Set_myplan()
 	myplan.request.t_0 = 0;
 	int pnt = mydemo.request.demo.points.size();
 	myplan.request.goal = mydemo.request.demo.points[pnt-1].positions;
-	myplan.request.goal_thresh.push_back(0.01);
-	myplan.request.goal_thresh.push_back(0.01);
-	myplan.request.goal_thresh.push_back(0.01);
+	myplan.request.goal_thresh.push_back(10);
+	myplan.request.goal_thresh.push_back(10);
+	myplan.request.goal_thresh.push_back(10);
 	myplan.request.tau = mydemo.response.tau;
 	myplan.request.seg_length = myplan.request.tau;
 	myplan.request.dt = mydemo.response.tau/(pnt-1);
 	myplan.request.integrate_iter = 1;
 
 	myplan.response.at_goal = false;
+	myplan.response.plan = raw_plan;
 }
 
 void Set_myactive_dmp()
@@ -108,51 +153,53 @@ int main(int argc, char **argv)
 	Set_myplan();
 
 	ROS_INFO("planCallback now begin");
-	generatePlan(active_dmp_list, myplan.request.x_0, myplan.request.x_dot_0, myplan.request.t_0, myplan.request.goal, myplan.request.goal_thresh,
+	generatePlan(mydemo.response.dmp_list, myplan.request.x_0, myplan.request.x_dot_0, myplan.request.t_0, myplan.request.goal, myplan.request.goal_thresh,
 			     myplan.request.seg_length, myplan.request.tau, myplan.request.dt, myplan.request.integrate_iter, myplan.response.plan, myplan.response.at_goal);
 	ROS_INFO("planCallback now ready");	
 	
 	SetActiveDMP();
 	
-	ROS_INFO("SetActiveDMP now begin");
+	//ROS_INFO("SetActiveDMP now begin");
 	active_dmp_list = myactive_dmp.request.dmp_list;
 	myactive_dmp.response.success = true;
-	ROS_INFO("SetActiveDMP now ready");
+	//ROS_INFO("SetActiveDMP now ready");
 
-	ROS_INFO("if_success: %d", myactive_dmp.response.success);
+	//ROS_INFO("if_success: %d", myactive_dmp.response.success);
 	ROS_INFO("if_at_goal: %d", myplan.response.at_goal);
 	ROS_INFO("the size of points: %d", myplan.response.plan.points.size());
+	//ROS_INFO("the first point: (%lf %lf %lf)",myplan.response.plan.points[0].positions[0],myplan.response.plan.points[0].positions[1],myplan.response.plan.points[0].positions[2]);
     
+
     while (ros::ok())
     {
 
     //创建一个 visualization_msgs/Marker消息，并且初始化所有共享的数据。消息成员默认为0，仅仅设置位姿成员w。
-    visualization_msgs::Marker points, line_strip, line_list;
-    points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "/my_frame";
-    points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
-    points.ns = line_strip.ns = line_list.ns = "dmps";
-    points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
-    points.pose.orientation.w = line_strip.pose.orientation.w = line_list.pose.orientation.w = 1.0;
+    visualization_msgs::Marker points, line_strip;
+    points.header.frame_id = line_strip.header.frame_id = "/my_frame";
+    points.header.stamp = line_strip.header.stamp = ros::Time::now();
+    points.ns = line_strip.ns = "dmps";
+    points.action = line_strip.action = visualization_msgs::Marker::ADD;
+    points.pose.orientation.w = line_strip.pose.orientation.w = 1.0;
 
 
     //分配三个不同的id到三个markers。points_and_lines名称空间的使用确保彼此不会相互冲突。
     points.id = 0;
     line_strip.id = 1;
-    line_list.id = 2;
+   
 
 
     //设置marker类型到 POINTS, LINE_STRIP 和 LINE_LIST
     points.type = visualization_msgs::Marker::POINTS;
     line_strip.type = visualization_msgs::Marker::LINE_STRIP;
-    line_list.type = visualization_msgs::Marker::LINE_LIST;
+    
 
 
     // scale成员对于这些marker类型是不同的,POINTS marker分别使用x和y作为宽和高，然而LINE_STRIP和LINE_LIST marker仅仅使用x，定义为线的宽度。单位是米。
-    points.scale.x = 0.05;
-    points.scale.y = 0.05;
+    points.scale.x = 10;
+    points.scale.y = 10;
 
     // LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
-    line_strip.scale.x = 0.05;
+    line_strip.scale.x = 10;
 
     // 点为绿色
     points.color.g = 1.0f;
@@ -165,9 +212,14 @@ int main(int argc, char **argv)
     for(int i=0; i<myplan.response.plan.points.size(); i++)
     {	
     	geometry_msgs::Point p;
-    	p.x = myplan.response.plan.points[i].positions[0];
-    	p.y = myplan.response.plan.points[i].positions[1];
-    	p.z = myplan.response.plan.points[i].positions[2];
+    	//cout << "(" << myplan.response.plan.points[i].positions[0]<< ","<<myplan.response.plan.points[i].positions[1] << ","<< myplan.response.plan.points[i].positions[2] << ")" <<endl;
+    	p.x = myplan.response.plan.points[i].positions[0]-2500;
+    	p.y = myplan.response.plan.points[i].positions[1]-2000;
+    	p.z = myplan.response.plan.points[i].positions[2]-3000;    	
+    	// p.x = mydemo.request.demo.points[i].positions[0]-2500;
+    	// p.y = mydemo.request.demo.points[i].positions[1]-2000;
+    	// p.z = mydemo.request.demo.points[i].positions[2]-3000;
+    	//ROS_WARN("the points: (%lf,%lf,%lf)",p.x,p.y,p.z);
 
     	points.points.push_back(p);
         line_strip.points.push_back(p);
@@ -177,7 +229,6 @@ int main(int argc, char **argv)
     //发布各个markers
     marker_pub.publish(points);
     marker_pub.publish(line_strip);
-    marker_pub.publish(line_list);
 
     r.sleep();
 
